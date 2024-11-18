3817829fb0912bae6b4e850b4e000eac4736016f
william kolosque <williammaich@hotmail.com>
2024-11-18 17:30:12 -0300

    Primeiro commit com banco de dados, schema e diagramas de classe e relacionamento.

---
+++ src/main/java/com/example/Banco_Magalu/BancoMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.example.Banco_Magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Banco_Magalu/BancoMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Banco_Magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
